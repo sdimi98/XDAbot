62dbe17bad192395cf951f89514f7981c73c5e15
sdimi98 <Sup3rrXDA1@gmail.com>
2024-05-07 14:26:59 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const {Client, IntentsBitField} = require('discord.js');
+const client = new Client({
+    intents:[
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+
+    ]
+})
