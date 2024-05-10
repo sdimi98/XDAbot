e835d7f88fee4025404f3692067dc73f145e8770
sdimi98 <Sup3rrXDA1@gmail.com>
2024-05-10 15:32:37 +0300

    dotenv integrated for secure token management

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const {Client, IntentsBitField} = require('discord.js');
+require('dotenv').config();
 const intents = require('./utils/intents');
 const client = new Client({
     intents:intents(),
 });
+client.login(process.env.TOKEN);
