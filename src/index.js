89ea695d9e4b69dc227dc4d1d15c452fc61192f2
sdimi98 <Sup3rrXDA1@gmail.com>
2024-05-10 15:23:28 +0300

    Intents moved to a separate file .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 const {Client, IntentsBitField} = require('discord.js');
+const intents = require('./utils/intents');
 const client = new Client({
-    intents:[
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-
-    ]
-})
+    intents:intents(),
+});
---
+++ src/utils/intents.js
@@ -0,0 +1,9 @@
+const {IntentsBitField}  = require('discord.js');
+module.exports=()=>{
+    return [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ];
+}
