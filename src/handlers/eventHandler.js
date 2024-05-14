1ed9e4c315127b0d872f64f77636496d289ab153
sdimi98 <Sup3rrXDA1@gmail.com>
2024-05-14 15:34:25 +0300

    Module compatibility issue resolved in package.json. Added eventHandler.js and the bot can now go online.

---
+++ src/handlers/eventHandler.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const getAllFiles = require('../utils/getAllFiles')
+module.exports = (client) => {
+    const eventFolders = getAllFiles(path.join(__dirname,'..','events'),true);
+    for (const eventFolder of eventFolders){
+        const eventFiles = getAllFiles(eventFolder);
+        // console.log(eventFiles);
+        const eventName = eventFolder.split('\\').pop();
+        client.on(eventName, async (arg)=>{
+            for (const eventFile of eventFiles){
+                const eventFunction = require(eventFile);
+                await eventFunction(client,arg);
+            }
+        })
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const {Client, IntentsBitField} = require('discord.js');
+const {Client} = require('discord.js');
 require('dotenv').config();
 const intents = require('./utils/intents');
+const eventHandler = require('./handlers/eventHandler');
 const client = new Client({
     intents:intents(),
 });
+eventHandler(client);
 client.login(process.env.TOKEN);
