45f565a034ed62f42e13342a3b8dd74365c89889
sdimi98 <Sup3rrXDA1@gmail.com>
2024-05-31 00:41:51 +0300

    Command handler implemented. Bot is now capable of executing commands.

---
+++ src/events/interactionCreate/handleCommands.js
@@ -0,0 +1,34 @@
+const {devs, server} = require('../../../config.json');
+const getLocalCommands = require('../../utils/getLocalCommands');
+
+module.exports = async (client, interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const localCommands = getLocalCommands();
+
+    try {
+        const commandObject = localCommands.find(
+            (cmd) => cmd.name === interaction.commandName
+        );
+
+        if (!commandObject) return;
+
+
+        if (commandObject.permissionsRequired?.length) {
+            for (const permission of commandObject.permissionsRequired) {
+                if (!interaction.member.permissions.has(permission)) {
+                    interaction.reply({
+                        content: 'Not enough permissions.',
+                        ephemeral: true,
+                    });
+                    return;
+                }
+            }
+        }
+
+
+        await commandObject.callback(client, interaction);
+    } catch (error) {
+        console.log(`There was an error running this command: ${error}`);
+    }
+};
