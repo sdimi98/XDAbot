5e8bfc25d33c94be6607da65f8503189d6f98d52
sdimi98 <Sup3rrXDA1@gmail.com>
2024-06-02 21:27:04 +0300

    Further improved the dice command by adding an optional maximum value for the dice.

--- src/commands/dice.js
+++ src/commands/dice.js
@@ -1,8 +1,25 @@
+const {ApplicationCommandOptionType} = require("discord.js");
 module.exports = {
-    name:'dice',
-    description:'dicee',
+    name: 'dice',
+    description: 'Rolls a dice from 1 to the max value is set. Default max value is 6.',
+    options: [
+        {
+            name: 'dice-size',
+            description: 'The maximum value for the dice.',
+            type: ApplicationCommandOptionType.Number,
+        }
+    ],
     permissionsRequired: [],
-    callback: (client,interaction) =>{
-        interaction.reply(Math.ceil(Math.random() * 6).toString())
+    callback: (client, interaction) => {
+        let max = 6;
+        if (!interaction.options.get('dice-size') === false){
+            if (interaction.options.get('dice-size').value <= 0){
+                interaction.reply('Please enter a valid number.')
+                return;
+            }
+            max = interaction.options.get('dice-size').value;
+        }
+
+        interaction.reply(Math.ceil(Math.random() * max).toString())
     }
 };
